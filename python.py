7739caab683460ef22fab260d5fcf7585ef1e4b0
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2026-09-01 06:00:04 +0800

    Python roadmap Exceptions lesson

--- python.py
+++ python.py
@@ -1,141 +1,138 @@
-# Make a personal expense tracker where:
-# This tracker allows you to add an expense
-# This tracker allows you to view your expenses
-# This tracker allows you to calculate the total expenses
-# This tracker allows you to show expenses by category
-# Allows you to exit the tracker
+# command-line ATM where the user can
+# 1. Check their balanca
+# 2. Deposit money
+# 3. Withdraw money
+# 4. Exit
 
-# This section displays the greeting message to the user
-print("Welcome to your Personal Expense Tracker!")
+# This section handles the initialization / definition of variables
+user_balance = 0.0 # initialized the overall bank balance of the user
+deposited = 0.0 # initialized the amount deposited by the user
+withdrawn = 0.0 # initialized the amount that can be withdrawn by the user
+
+# This section handles the definition of custom exceptions
+class DepositError(Exception):
+    pass
+
+class WithdrawalError(Exception):
+    pass
+
+# This section handles the function definition of all the actions that the user can take.
+def check_balance(user_balance: float):
+    """
+    This function handles the display of the user's current balance to the terminal.
+
+    Args:
+        1. user_balance - float type variable representing the current bank balance of the user
+    Returns:
+        1. None - performs the displaying of the users balance only - does not return any value.
+    """
+
+    print("="*60)
+    print("You have opted to check your current balance.")
+    print("="*60)
+
+    # This section handles the display of the user balance to the user
+    print(f"Your current balance is: {user_balance}")
+
+def deposit_money(user_balance: float):
+    """
+    This function handles the depositing of money into the user's bank account.
+
+    Args:
+        1. user_balance - float type variable representing the current bank balance of the user
+    Returns:
+        1. deposit - represents the validated deposit amount by the user to be added to user_balance
+    """
+    print("="*60)
+    print("You have opted to deposit money to your bank balance.")
+    print("="*60)
+
+    # This section handles the prompting & validation of the users deposit amount
+    print("How much would you like to deposit into your bank account?")
+    try:
+        deposit = float(input())
+
+        if deposit < 0:
+            raise DepositError()
+        
+    except (ValueError, DepositError):
+        print("Please input an appropriate deposit amount!")
+        exit()
+    except Exception:
+        print("Something went wrong - please contact support!")
+        exit()
+
+    print(f"You have deposited {deposit}.")
+    return deposit
+
+def withdraw_money(user_balance: float):
+    """
+    This function handles the withdrawal of money from the user's bank account.
+
+    Args:
+        1. user_balance - float type variable representing the current bank balance of the user
+    Returns:
+        1. withdrawal - float type variable representing the validated withdrawn amount by the user to be subtracted from user_balance
+    """
+    print("="*60)
+    print("You have opted to withdraw money from your bank balance.")
+    print("="*60)
+
+    # This section handles the prompting & validation of the users withdrawal amount
+    print("How much would you like to withdraw from your bank account?")
+    try:
+        withdrawal = float(input())
+
+        if (withdrawal > user_balance) or (withdrawal < 0):
+            raise WithdrawalError()
+
+    except (ValueError, WithdrawalError):
+        print("Please input an appropriate withdrawal amount!")
+        exit()
+    except Exception:
+        print("Something went wrong - please contact support!")
+        exit()
+
+    print(f"You have withdrawn {withdrawal}")
+    return withdrawal
+
 print("="*60)
-print("1. Add Expense")
-print("2. View Expenses")
-print("3. Calculate Total Expenses")
-print("4. Show Expenses by Category")
-print("5. Exit Tracker")
+print("Welcome to the Terminal ATM! What would you like to do for today? Please input the appropriate numbers 1, 2, and 3.")
+print("="*60)
 
-expenses = {}
+# This section handles the display of the different possible user actions to the user through the terminal
+print("1. Check your current balance")
+print("2. Deposit money to your bank account")
+print("3. Withdraw money from your bank account")
+print("4. Exit")
 
-def add_expense(expenses: dict):
-    """
-    This function handles the addition of a new expense to existing expense list.
+# This section handles the prompting & validation of the user input for the choices
 
-    Args:
-        1. expenses - dict type parameter representing the dictionary variable expenses.
-    Returns:
-        1. None - only performs the appending of a new expense into the expenses dictionary.
-    """
-    print("You have chosen to add an expense!")
-
-    # This section handles the inputting of the description of the new expense
-    expense_description = input("What is the description of this expense? ")
-    print(f"Description: {expense_description}")
-
-    # This section handles the inputting of the category of the new expense
-    expense_category = input("What is the category of this expense? ")
-    print(f"Category: {expense_category}")
-
-    # This section handles the inputting of the amount of the new expense
-    expense_amount = input("What is the amount of this expense? ")
-    print(f"Amount: {expense_amount}")
-
-    # This section handles the inputting of the new expense to the expenses dictionary & its display
-    expenses[expense_description] = {"Category": expense_category, "Amount": expense_amount}
-
-    print(f"The expense for {expense_description} has just been added!")
-
-def view_expenses(expenses: dict):
-    """
-    This function handles the display of current expenses from the expense list.
-
-    Args:
-        1. expenses - dict type variable containing all of the expenses inputted by the user.
-    Returns:
-        1. None - function only performs the displaying of the expenses to the user - does not return anything.
-    """
-    print("You have chosen to view your current expenses!")
-
-    # This section handles the display of all of the expenses and its corresponding category & amounts from the expenses dictionary
-    for expense in expenses:
-        print(f"Description: {expense}")
-        print(f"Category: {expenses[expense]["Category"]}")
-        print(f"Amount: {expenses[expense]["Amount"]}")
-        print("-"*30)
-
-def calculate_expenses(expenses: dict):
-    """
-    This function calculates the total expenses using the price from the expense list.
-    
-    Args:
-        1. expenses - dict type variable containing all of the expenses inputted by the user.
-    Returns:
-        1. None - function only performs the calculation of the total expense and its display to the user - does not return anything.
-    """
-    print("You have chosen to calculate the total expenses!")
-
-    running_expense_total = 0
-
-    # This section handles the calculation of the total expenses given the added expenses from the expenses dictionary
-    for expense in expenses:
-        current_expense_amount = int(expenses[expense]["Amount"])
-        running_expense_total += current_expense_amount
-
-    print(f"Your total expense so far is {running_expense_total}!")
-
-
-def show_expenses_category(expenses: dict):
-    """
-    This function handles the display of expenses given a category.
-
-    Args:
-        1. expenses - dict type variable containing all of the expenses inputted by the user.
-    Returns:
-        1. None - function only performs the display of all of the different expense categories & the expenses under the chosen expense category
-    """
-    print("You have chosen to show the expenses by category")
-
-    # This section handles the display of all of the different categories of expenses
-    print("Here are the current categories of your expenses so far:")
-
-    expense_counter = 1
-    expense_categories = []
-
-    for expense in expenses:
-        category_to_display = expenses[expense]["Category"]
-
-        if category_to_display not in expense_categories:
-            print(f"{expense_counter}. {category_to_display}")
-            expense_categories.append(category_to_display)
-            expense_counter += 1
-
-    # This section handles prompting the user of what categories' expenses needs to be displayed
-    user_choice = input("Please input the name of the category you would like to see the expenses of ")
-
-    for expense in expenses:
-        if user_choice not in expense_categories:
-            print("Please input a proper category.")
-            break
-        else:
-            if (expenses[expense]["Category"] in expense_categories) and (expenses[expense]["Category"] == user_choice):
-                print(f"Description: {expense}")
-                print(f"Category: {expenses[expense]["Category"]}")
-                print(f"Amount: {expenses[expense]["Amount"]}")
-                print("-"*30)        
-
-# This section handles the prompting of user's choice for action
 while(True):
-    user_choice = int(input("What action would you like to do right now? "))
+    try:
+        user_choice = int(input("What would you like to do? "))
+    except ValueError:
+        print("Please input an appropriate number!")
+        exit()
+    except Exception:
+        print("Something went wrong - please contact support!")
+        exit()
 
+    # This section handles the calling of the appropriate functions depending on user choice
     match user_choice:
         case 1:
-            add_expense(expenses)
+            check_balance(user_balance)
         case 2:
-            view_expenses(expenses)
+            deposited = deposit_money(user_balance)
+            user_balance = user_balance + deposited
+            print(f"Your new balance is now {user_balance}.")
         case 3:
-            calculate_expenses(expenses)
+            withdrawn = withdraw_money(user_balance)
+            user_balance = user_balance - withdrawn
+            print(f"Your new balance is now {user_balance}.")
         case 4:
-            show_expenses_category(expenses)
-        case 5:
-            print("You have chosen to exit your personal expenses tracker, goodbye!")
+            print("Thank you for using the command-line ATM!")
             exit()
-
+        case _:
+            print("Please choose an appropriate number!")
+            exit()
