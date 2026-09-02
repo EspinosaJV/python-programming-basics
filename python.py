cfb6bf7f4a705baaa0ccc82b90fa899d1e19db10
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2026-09-03 05:33:23 +0800

    Python Roadmap, Google for Education Python, Lists

--- python.py
+++ python.py
@@ -1,84 +1,55 @@
-s = 'hi'
-print(s[1]) ## i
-print(len(s)) ## 2
-print(s + ' there') ## hi there
+# Python Lists
 
-pi = 3.14
-## text = 'The value of pi is ' + pi ## NO, does not work
-text = 'The value of pi is ' + str(pi)
-print(text)
+colors = ['red', 'blue', 'green']
+print(colors[0]) ## red
+print(colors[2]) ## green
+print(len(colors)) ## 3
 
-raw = r'this\t\n and that'
+# FOR and IN
+squares = [1, 4, 9, 16]
+sum = 0
+for num in squares:
+    sum += num
+print(sum) ## 30
 
-# this\t\n and that
-print(raw)
+list = ['larry', 'curly', 'moe']
+if 'curly' in list:
+    print('yay') ## yay
 
-multi = """It was the best of times.
-It was the worst of times."""
+## print the numbers from 0 through 99
+for i in range(100):
+    print(i)
 
-# It was the best of times.
-#   It was the worst of times.
-print(multi)
+# While Loop
+## Access every 3rd element in a list
+i = 0
+while i < len(a):
+    print(a[i])
+    i = i + 3
 
-# String Slicing
-s = "Hello"
-print(s[1:4]) # ell
-print(s[1:]) # ello
-print(s[:]) # Hello
-print(s[1:100]) # is 'ello'
+list = ['larry', 'curly', 'moe']
+list.append('shemp') ## append elem at end
+list.insert(0, 'xxx') ## insert elem at index 0
+list.extend(['yyy', 'zzz']) ## add list of elems at end
+print(list) ## ['xxx', 'larry', 'curly', 'moe', 'shemp', 'yyy', 'zzz']
+print(list.index('curly')) ## 2
 
-print(s[-1]) # 'o'
-print(s[-4]) # 'e' - 4th from the end
-print(s[:-3]) # 'He' - going up to but not including the last 3 characters
-print(s[-3:]) # 'llo' - starting with the 3rd character from the end and extending to the end of the string.
+list.remove('curly') ## search and remove that element
+list.pop(1) ## removes and returns ('larry')
+print(list) ## ['xxx', 'moe'. 'shemp', 'yyy', 'zzz']
 
-# String Formatting
-value = 2.791514
-print(f'approximate value = {value:.2f}') # approximate value = 2.79
+list = [1, 2, 3]
+print(list.append(4)) ## NO, does not work, append() returns None
+## Correct pattern:
+list.append(4)
+print(list)
 
-car = {'tires': 4, 'doors': 2}
-print(f'car = {car}') # car = {'tires': 4, 'doors': 2}
+list = [] ## Start as the empty list
+list.append('a') ## Use append() to add elements
+list.append('b') # Use append() to add another element
 
-address_book = [{'name': 'N.X.', 'addr': '15 Jones St', 'bonus': 70},
-                {'name': 'J.P.', 'addr': '1005 5th St', 'bonus': 400},
-                {'name': 'A.A.', 'addr': '200001 Bdwy', 'bonus': 5},]
-
-for person in address_book:
-    print(f'{person["name"]:8} || {person["addr"]:20} || {person["bonus"]:>5}')
-
-# String %
-
-# % operator
-text = "%d little pigs come out, or I'll %s, and I'll %s, and I'll blow your %s down." % (3, 'huff', 'puff', 'house')
-
-# Add parentheses to make the long line work:
-text = (
-    "%d little pigs come out, or I'll %s, and I'll %s, and I'll blow your %s down."
-    % (3, 'huff', 'puff', 'house'))
-
-# Split the line into chunks, which are concatenated automatically by Python
-# text = (
-#     "%d little pigs come out, "
-#     "or I'll %s, and I'll %s, "
-#     "and I'll blow your %s down."
-#     %s (3, 'huff', 'puff', 'house'))
-
-ustring = 'A unicode \u018e string \xf1'
-b = ustring.encode('utf-8')
-print(b) ## bytes of utf-8 encoding. Note the b prefix.
-t = b.decode('utf-8') ## convert bytes back to a unicode string
-print(t)    ## it's the same as the original, yay!
-print(t == ustring)
-
-# If Statements
-if time_hour >= 0 and time_hour <= 24:
-    print('Suggesting a drink option...')
-    if mood == 'sleepy' and time_hour < 10:
-        print('coffee')
-    elif mood == 'thirsty' or time_hour < 2:
-        print('lemonade')
-    else:
-        print('water')
-
-if time_hour < 10: print('coffee')
-else: print('water')
+# List Slices
+list = ['a', 'b', 'c', 'd']
+print(list[1:-1]) ## ['b', 'c']
+list[0:2] = 'z' ## replace ['a', 'b'] with ['z']
+print(list) ## ['z', 'c', 'd']
