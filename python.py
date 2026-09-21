108c94bf53e006fe1ad810ca2bfcddc73edc82d5
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2026-09-21 23:39:28 +0800

    Python for Beginners: The Complete Course, Chapter 6

--- python.py
+++ python.py
@@ -1,83 +1,102 @@
-## CHAPTER 5 - TESTING & DEBUGGING
+## CHAPTER 6 - COMPUTING
 
-# tests the functionality of the code we write - unit tests
-# we write inputs to the code that we write expecting outputs - unit tests
-
-def total_xp(level, xp_to_add):
-    pass
-
-# unit tests rpovide run cases that provide different sets of inputs
-# unit tests also provide submit cases that provide different sets of input swhen we decide to submit a code
-
-# we want to test our code bit by bit - methodical with how we test things
-
-# output testing - just checking the outputs of our code
-# unit testing - inputting different values into our code to see how they are processed
-
-## Assignment
-# complete the total_xp function. It accepts two integers as input:
-# level and xp_to_add
-# There are 100 xp per level and total_xp should convert the current level to xp, then add this current xp to the xp_to_add argument and return the player's total xp. For example:
-# If a player is level 1 and gains 100 xp, they have 200 total xp.
-# If a player is level 2 and gains 250 xp, they have 450 total xp.
-# If a player is level 170 and gains 590 xp, they have 17590 total xp.
-
-def total_xp(level, xp_to_add):
-    base_xp = level * 100
-    total = base_xp + xp_to_add
-
-    return total
-
-# Debugging
-# it's best practice to debug code before making it go live
-
-def take_magic_damage(health, resist, amp, spell_power):
-    total_damage = spell_power * amp
-    actual_damage_dealt = total_damage - resist
-    new_health = health - actual_damage_dealt
-
-    return new_health
+# basic math operations of addition, subtraction, multiplication, and division
+# if done on integers = integer result
+# if done on floats = float type results
 
 # Assignment
-# Complete the take_magic_damage function. It should return the new_health after calculating how much magic-type damage the player takes. Here is a description of the arguments:
-# health: the player's starting health
-# resist: the player's magic resistance. This reduces the damage they take by a static amount
-# amp: the attacker's magic amplification. This increases the damage they deal by a damage multiplier
-# spell_power: the base damage of the spell
-# first, calculate the total maximum damage to be inflicted by multiplying the spell_power by the amp. Then, subtract the resist from the total damage to get the actual damage dealt. Apply that damage to the player's health and return the new health.
+# Complete the missing sections of the calculate_damage function.
+    # Fix the total_damage variable so that it contains the sum of all the different weapons' damage values
+    # Fix the average_damage variable so that it contains the average weapon damage
 
-# Learning effectively
-# First read through the whole lesson & understand the lesson
-# For each assignment - read through whole assignment & understand the assignment
-# Add print statements to code to help debug
+def calculate_damage(sword, arrow, spear, dagger, fire):
+    total_damage = sword + arrow + spear + dagger + fire
+    average_damage = total_damage / 5
 
-# Debugging Practice
+    return total_damage, average_damage
 
-def unlock_achievement(before_xp, ach_xp, ach_name):
-    after_xp = before_xp + ach_xp
-    print(f"After XP = {after_xp}")
+# integers and floats are numerical types in Python
 
-    alert = f"Achievement Unlocked: {ach_name}"
-    print(f"Alert string = {alert}")
-    return after_xp, alert
+## Floor Division = //
+# if we give it 2 integers - it will always output an integer type, chopping off decimal points
+# 7 // 3
+# 2 (an integer)
+# Floor of a number is the LARGEST integer which is less than or equal to the number
+
+# 9 / -4 = -2.25
+# but with floor division, 9 // -4 = -3 (remember floor is the LARGEST integer which is less than or equal to the number) (-2.25 rounds down to the floor which is -3)
+
+# result of 11 / 2 - actual answer is 5.5 but for 11 / 2 this is 5
+
+## Exponents - **
+
+expo = 9 ** 2
+
+print(f" EXPO: {expo}") # the answer is 81 or 9 * 9
+
+# 2 ** 3 = 8
+
+## Changing in Place
+# You can change a variable in place
+# Variables can be used in the left & right hand side of the equal sign
+
+def update_player_score(current_score, increment):
+    print(f"Current score before changing in place: {current_score}")
+    current_score = current_score + 300 # we are resetting the value of the variable current score to the same value + 300
+    # we are assigning current_score value + 300 to the current_score variable once again, changing in place
+    print(f"Current score after changing in place: {current_score}")
 
 # Assignment
-# Let's complete the unlock_achievement function. It accepts 3 arguments:
-    # before_xp - int
-    # ach_xp - int
-    # ach_name - str
+# Complete the update_player_score function. It should add increment to current_score and then return the new current_score
 
-# It should return 2 values:
-    # The player's xp after the achievement is unlocked (The sum of before_xp and ach_xp)
-    # An alert message that says "Achievement Unlocked: ACHIEVEMENT_NAME", WHERE ACHIEVEMENT_NAME is the name of the achievement
+def update_player_score(current_score, increment):
+    current_score = current_score + increment
+    return current_score
 
-# Stack Trace
-# Also known as Traceback
-# Tells us that there are errors in our different files
-# Python treats whitespace as meaningful - indents needs to be 4 spaces long
-# 
+## Plus Equals = +=
+# Also known as in place operators (-=, /=, *=)
 
-def create_stats_message(strength, wisdom, dexterity):
-    total = strength + wisdom + dexterity
-    msg = f"You have {strength} stength, {wisdom} wisdom, and {dexterity} dexterity for a total of {total} stats."
-    return msg
+def get_hurt(current_health, damage):
+    print(f"Current Health: {current_health}")
+    current_health += 77
+    print(f"Current Health after: {current_health}")
+    current_health *= 77
+    print(f"Current Health after: {current_health}")
+    current_health /= 77
+    print(f"Current Health after: {current_health}")
+
+# Assignment
+# Complete the get_hurt function. It should use the -= in-place operator to subtract damage from current_health and then return the new current_health.
+
+def get_hurt(current_health, damage):
+    current_health -= damage
+    return current_health
+
+# Binary Notation
+
+can_create_guild = 0b1000
+can_review_guild = 0b0100
+can_delete_guild = 0b0010
+can_edit_guild = 0b0001
+
+def get_create_bits(user_permissions):
+    permission = user_permissions & can_create_guild
+    return permission
+
+def get_review_bits(user_permissions):
+    return user_permissions & can_review_guild
+
+def get_delete_bits(user_permissions):
+    return user_permissions & can_delete_guild
+
+def get_edit_bits(user_permissions):
+    return user_permissions & can_edit_guild
+
+## Bitwise | Operator
+# Assignment
+# Complete the calculate_guild_perms function. It should return a binary number that represents the permissions of all the members of the guild (Glorfindel, Galadriel, Elendil, and Elrond).
+# Use a series of bitwise "or" operations to calculate the superset of all the member's permissions
+
+def calculate_guild_perms(glorfindel, galadriel, elendil, elrond): 
+    party_permissions = glorfindel | galadriel | elendil | elrond
+    return party_permissions
