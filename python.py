caf0d2b214155c3377a56089ea56034285367bbd
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2026-09-23 22:36:16 +0800

    Python for Beginners: The Complete Course, Chapter 8 - Loops

--- python.py
+++ python.py
@@ -1,102 +1,175 @@
-## CHAPTER 6 - COMPUTING
+## CHAPTER 7 COMPARISONS
 
-# basic math operations of addition, subtraction, multiplication, and division
-# if done on integers = integer result
-# if done on floats = float type results
+# Comparison Operators
+# Boolean logic & Comparison operators
+# Comparing logic that results in either True or False
 
-# Assignment
-# Complete the missing sections of the calculate_damage function.
-    # Fix the total_damage variable so that it contains the sum of all the different weapons' damage values
-    # Fix the average_damage variable so that it contains the average weapon damage
-
-def calculate_damage(sword, arrow, spear, dagger, fire):
-    total_damage = sword + arrow + spear + dagger + fire
-    average_damage = total_damage / 5
-
-    return total_damage, average_damage
-
-# integers and floats are numerical types in Python
-
-## Floor Division = //
-# if we give it 2 integers - it will always output an integer type, chopping off decimal points
-# 7 // 3
-# 2 (an integer)
-# Floor of a number is the LARGEST integer which is less than or equal to the number
-
-# 9 / -4 = -2.25
-# but with floor division, 9 // -4 = -3 (remember floor is the LARGEST integer which is less than or equal to the number) (-2.25 rounds down to the floor which is -3)
-
-# result of 11 / 2 - actual answer is 5.5 but for 11 / 2 this is 5
-
-## Exponents - **
-
-expo = 9 ** 2
-
-print(f" EXPO: {expo}") # the answer is 81 or 9 * 9
-
-# 2 ** 3 = 8
-
-## Changing in Place
-# You can change a variable in place
-# Variables can be used in the left & right hand side of the equal sign
-
-def update_player_score(current_score, increment):
-    print(f"Current score before changing in place: {current_score}")
-    current_score = current_score + 300 # we are resetting the value of the variable current score to the same value + 300
-    # we are assigning current_score value + 300 to the current_score variable once again, changing in place
-    print(f"Current score after changing in place: {current_score}")
+# <, >, <=, >=, ==, !=
 
 # Assignment
-# Complete the update_player_score function. It should add increment to current_score and then return the new current_score
+# Complete the player_1_wins function. It should return True if player 1 has a high score and False otherwise
 
-def update_player_score(current_score, increment):
-    current_score = current_score + increment
-    return current_score
+def player_1_wins(player_1_score, player_2_score):
+    return player_1_score > player_2_score
 
-## Plus Equals = +=
-# Also known as in place operators (-=, /=, *=)
+# Sample Code
 
-def get_hurt(current_health, damage):
-    print(f"Current Health: {current_health}")
-    current_health += 77
-    print(f"Current Health after: {current_health}")
-    current_health *= 77
-    print(f"Current Health after: {current_health}")
-    current_health /= 77
-    print(f"Current Health after: {current_health}")
+user_username = 'nate_codes'
+user_password = 'password12345'
+
+def check_login_credentials(login_username, login_password):
+    usernames_match = login_username == user_username
+
+    passwords_match = login_password == user_password
+
+    both_match = usernames_match & passwords_match
+    return both_match
+
+login_username = 'nate_codes'
+login_password = 'WoWisALameGame!'
+
+login_successful = check_login_credentials(login_username, login_password)
+print(f"Login Successful: {login_successful}")
 
 # Assignment
-# Complete the get_hurt function. It should use the -= in-place operator to subtract damage from current_health and then return the new current_health.
+# Create the following variables. Use comparison operators to determine their boolean values. The context of the parameter names should tell you how to make these comparisons. Return them in this order:
 
-def get_hurt(current_health, damage):
-    current_health -= damage
-    return current_health
+# 1. is_mustang_edward_same
+# 2. is_alphonse_edward_same
+# 3. is_winry_alphonse_same
 
-# Binary Notation
+def compare_heights(edward_height, alphonse_height, winry_height, mustang_height):
+    is_mustang_edward_same = mustang_height == edward_height
+    is_alphonse_edward_same = alphonse_height == edward_height
+    is_winry_alphonse_same = winry_height == alphonse_height
 
-can_create_guild = 0b1000
-can_review_guild = 0b0100
-can_delete_guild = 0b0010
-can_edit_guild = 0b0001
+    return is_mustang_edward_same, is_alphonse_edward_same, is_winry_alphonse_same
 
-def get_create_bits(user_permissions):
-    permission = user_permissions & can_create_guild
-    return permission
-
-def get_review_bits(user_permissions):
-    return user_permissions & can_review_guild
-
-def get_delete_bits(user_permissions):
-    return user_permissions & can_delete_guild
-
-def get_edit_bits(user_permissions):
-    return user_permissions & can_edit_guild
-
-## Bitwise | Operator
 # Assignment
-# Complete the calculate_guild_perms function. It should return a binary number that represents the permissions of all the members of the guild (Glorfindel, Galadriel, Elendil, and Elrond).
-# Use a series of bitwise "or" operations to calculate the superset of all the member's permissions
+# Complete the can_withstand_blow function. It should return True if the hero's armor is greater than or equal to the damage dealt by the enemy, and False otherwise.
 
-def calculate_guild_perms(glorfindel, galadriel, elendil, elrond): 
-    party_permissions = glorfindel | galadriel | elendil | elrond
-    return party_permissions
+def can_withstand_blow(hero_armor, enemy_damage):
+    return hero_armor >= enemy_damage
+
+# If Statements
+def print_status(player_health):
+    # ?
+
+# Don't edit below this line
+
+def test(health):
+    print(f"Player Health: {health}")
+
+# The condition should result to a truthy value 
+# empty string (''), 0 - Falsy, 1 - Truthy
+
+# Assignment
+# Complete the print_status function
+# If player_health is 0, print the text dead to the console
+# Afterwards, whether or not the player is dead, print the text status check complete to the console
+
+def print_status(player_health):
+
+    if player_health == 0:
+        print("dead")
+
+    print("status check complete")
+
+# Don't edit below this line
+
+def test(health):
+    print(f"Player Health: {health}")
+    print("Checking status...")
+    print_status(health)
+    print("========================")
+
+def main():
+    test(0)
+    test(5)
+
+# Assignment
+# Complete the check_swords_for_army function. If the number of swords and the number of soldiers match, return the string correct amount, otherwise, return the string incorrect amount
+
+def check_swords_for_army(number_of_swords, number_of_soldiers):
+    if number_of_swords == number_of_soldiers:
+        return "correct amount"
+
+    return "incorrect amount"
+
+# Another assignment
+# Complete the player_status function. If the player health is less than or equal to 0, return the string dead, otherwise, if it's less than or equal to 5, return the string injured, otherwise, return the string healthy
+
+def player_status(health):
+    if health <= 0:
+        return "dead"
+    elif health <= 5:
+        return "injured"
+    else:
+        return "healthy"
+
+# Another assignment
+# There is a bug in the check_high_score function! Add the proper conditional statement to fix the bug. If the names match, "You are the highest scoring player!" should be returned. Otherwise, "You are not the highest scoring player!" should be returned.
+
+def check_high_score(current_player_name, high_scoring_player_name):
+    if current_player_name == high_scoring_player_name:
+        return "You are the highest scoring player!"
+    else:
+        return "You are not the highest scoring player!"
+
+# Another assignment
+# Complete the check_high_score function. If the player_name matches the high score name, return the string high, otherwise if it's the low scorer, return the string low, otherwise return the string neither
+
+def check_high_score(player_name, high_scoring_player_name, low_scoring_player_name)
+    if player_name == high_scoring_player_name:
+        return "high"
+    elif player_name == low_scoring_player_name:
+        return "low"
+    else:
+        return "neither"
+
+# Boolean Logic
+
+# Another assignment
+# We need a way for our game to track whether a character's attack hits or misses
+# Complete the does_attack_hit function. The function should return True if either of the following conditions are met:
+    # The attack_roll is not a 1 and the attack roll is greater than or equal to the armor_class, or
+    # The attack roll is a 20
+# Otherwise, it should return False
+
+def does_attack_hit(attack_roll, armor_class):
+    if attack_roll != 1 and attack_roll >= armor_class or attack_roll == 20:
+        return True
+    else:
+        return False
+
+    # or you can do
+    condition_1 = attack_roll != 1
+    condition_2 = attack_roll >= armor_class
+    condition_3 = attack_roll == 20
+
+    result = (condition_1 and condition_2) or condition_3
+    return result
+
+# Another assignment
+# In Fantasy Quest, players can go to a town's local pub. Drinking virtual beer refills their stamina!
+# Complete the function that determines if a bartender should server drinks to a customer. Only return True if all of these conditions apply:
+    # The customer's age is 21 or older
+    # The bartender is working
+    # The time is at least 5 but no later than 10
+
+def should_serve_customer(customer_age, on_break, time):
+    condition_1 = customer_age >= 21
+    condition_2 = on_break == False
+    condition_3 = 5 >= time <= 10
+
+    if condition_1 and condition_2 and condition_3:
+        return True
+
+    # or
+
+    condition_1 = customer_age >= 21
+    # condition_2 = on_break
+    condition_3 = 5 <= time <= 10
+
+    return condition_1 and not on_break and condition_3
+
